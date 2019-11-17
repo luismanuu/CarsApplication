7f7e194622ec1b3bde75f22e08c33ffed3d3bf4f
Luis Yagual <lucho15465@outlook.com>
2019-11-17 00:47:10 -0500

    Creación de database mediante migration

--- CarsApplication/Persistence/AppDbContext.cs
+++ CarsApplication/Persistence/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CarsApplication.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,7 @@ namespace CarsApplication.Persistence
         {
 
         }
+
+        public DbSet<Make> Makes { get; set; }
     }
 }
