130df64e25201391cbb0c2c469735a258e6810e9
Luis Yagual <lucho15465@outlook.com>
2019-11-19 20:44:06 -0500

    Creación de componente vehicle-form, rutas. html inicial

---
+++ CarsApplication/ClientApp/src/app/vehicle-form/vehicle-form.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { VehicleFormComponent } from './vehicle-form.component';
+
+let component: VehicleFormComponent;
+let fixture: ComponentFixture<VehicleFormComponent>;
+
+describe('vehicle-form component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ VehicleFormComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(VehicleFormComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ CarsApplication/ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-vehicle-form',
+    templateUrl: './vehicle-form.component.html',
+    styleUrls: ['./vehicle-form.component.css']
+})
+/** vehicle-form component*/
+export class VehicleFormComponent {
+    /** vehicle-form ctor */
+    constructor() {
+
+    }
+}
